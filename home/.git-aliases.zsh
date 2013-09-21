c814115b1e37cdfed98784ca755ec626c7f0b1c6
Jeffrey Horn <jrhorn424@gmail.com>
2013-09-20 20:50:54 -0400

    Add nice git aliases

---
+++ home/.git-aliases.zsh
@@ -0,0 +1,21 @@
+# Use `hub` as our git wrapper:
+#   http://defunkt.github.com/hub/
+hub_path=$(which hub)
+if (( $+commands[hub] ))
+then
+  alias git=$hub_path
+fi
+
+# The rest of my fun git aliases
+alias gl='git pull --prune'
+alias glog="git log --oneline --graph --all --decorate --abbrev-commit --date=relative --pretty"
+alias gp='git push origin HEAD'
+alias gd='git diff'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias ga='git add '
+alias gcam='git commit -am'
