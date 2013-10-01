2f5c16fa833a35b2e60692c93dab2d2ef75152a6
Jeffrey Horn <jrhorn424@gmail.com>
2013-10-01 15:57:17 -0400

    Update configuration and aliases

--- home/.git-aliases.zsh
+++ home/.git-aliases.zsh
@@ -1,12 +1,40 @@
-alias gl='git pull --prune'
+#
+# pulling
+#
+alias glp='git pull --rebase --prune'
+alias gl='git pull --rebase'
+alias pull="git pull --rebase"
+
+#
+# pushing
+#
+alias gp='git rebase -i @{u} && git push origin HEAD'
+alias push='git rebase -i @{u} && git push origin HEAD'
+
+#
+# merging
+#
+alias gm="git merge -no-ff" # on master,  meaningfully merge feature branch
+alias merge="git merge -no-ff" # on master,  meaningfully merge feature branch
+
+#
+# inspection
+#
 alias glog="git log --oneline --graph --all --decorate --abbrev-commit --date=relative --pretty"
-alias gp='git push origin HEAD'
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gd='git diff'
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+
+#
+# adding and committing
+#
+alias ga='git add'
 alias gc='git commit'
 alias gca='git commit -a'
+alias gcam='git commit -am'
+
+#
+# branching and checkouts
+#
 alias gco='git checkout'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
-alias ga='git add '
-alias gcam='git commit -am'
