fbafb48503ef5d8ee0a2d72ab34dcbe7e7a65bc8
Jeffrey Horn <jeffh@generalassemb.ly>
2015-08-31 18:31:05 -0400

    Pretty log

--- home/.git-aliases.zsh
+++ home/.git-aliases.zsh
@@ -3,6 +3,7 @@
 #
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gd='git diff'
+alias gl="git log --oneline --abbrev-commit --all --graph --decorate --color --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
 
 #
 # adding and committing
