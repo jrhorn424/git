0793b261d168913663d385e449596dc2294283a0
Jeffrey Horn <jeff@jrhorn.me>
2017-06-13 16:05:27 -0400

    Remove aliases

--- home/.git-aliases.zsh
+++ home/.git-aliases.zsh
@@ -1,25 +1,4 @@
 #
-# inspection
-#
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gd='git diff'
-alias gl="git log --oneline --abbrev-commit --all --graph --decorate --color --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
-
-#
-# adding and committing
-#
-alias ga='git add'
-alias gc='git commit'
-alias gca='git commit -A .'
-alias gcam='git commit -Am'
-
-#
-# branching and checkouts
-#
-alias gco='git checkout'
-alias gb='git branch'
-
-#
 # convenience
 #
 function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
